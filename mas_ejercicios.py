25259a879c5949ecfe34f85f653ab2445c182b60
rafa <rafael.est07@gmail.com>
2026-04-20 09:12:42 -0400

    segundo commit

---
+++ mas_ejercicios.py
@@ -0,0 +1,12 @@
+
+entrenador = "jesi"
+pkm1 = 6
+
+entrenador2 = "Sabrina"
+pkm2 = 7
+
+if pkm2 <= 6:
+    print("Puede acceder a la liga Pokemon")
+else:
+    print("Tiene más Pokemons de los necesarios")
+
