abad9f400946fe2697e7afe7cab5fc614d4a5ada
rafa <rafael.est07@gmail.com>
2026-05-27 16:53:04 -0400

    LISTAS Y DICCIONARIOS

---
+++ colecciones/diccionarios.py
@@ -0,0 +1,37 @@
+#USO Y EXPLICACIÓN DE DICCIONARIOS
+
+# alumno = {
+#     "nombre" : "Kirbi",
+#     "edad" : 14,
+#     "carrera" : "Piloto"
+# }
+
+# print(alumno)
+# print(alumno["carrera"])
+
+# for key, value in alumno.items():
+#     print(key, value)
+
+# alumno["email"] = "kirbi@gmail.com"
+# alumno["carrera"] = "escritor"
+# del alumno["edad"]
+# for key, value in alumno.items():
+#     print(f"{key} = {value}")
+
+#DICCIONARIO CON DICCIONARIOS
+
+productos = {
+    1:{"nombre" : "Control Inalambrico",
+    "categoria" : "Electronica",
+    "precios" : 45000},
+    2:{"nombre" : "Pilas recargables",
+    "categoria" : "Insumos",
+    "precios" : 5000},
+    3:{"nombre" : "Pasta termica",
+    "categoria" : "Computación",
+    "precios" : 10000}
+}
+
+
+print(productos[0]["precios"])
+
---
+++ colecciones/listas.py
@@ -0,0 +1,47 @@
+#USO DE LISTAS
+
+# lista = [8, 20, 12, 87]
+
+# print(lista)
+# print(lista[3])
+
+# for elemento in lista:
+#     print(f"Número: {elemento}")
+
+
+# canasta = ["frutilla", "platano", "mora", "kiwi"]
+
+# for elemento in canasta:
+#     print(f"Fruta: {elemento}")
+
+'''
+Hacer una lista de nombres y otra de apellidos
+Mostrar las listas como si fueran nombres completos
+Vale decir, Diego Robles, Adolfo Hinako, Luis Mussolini
+'''
+
+# nombres = ["Pablo", "Felipe", "Maria"]
+# apellidos = ["González", "Hermosilla", "Martinez"]
+
+# nom = 0
+# ape = 0
+# while nom < 3:
+#     print(nombres[nom], apellidos[ape])
+#     nom += 1
+#     ape += 1
+
+# print(f"{nombres[0]} {apellidos[0]},{nombres[1]} {apellidos[1]},{nombres[2]} {apellidos[2]}")
+
+# for n in range(len(nombres)):
+#     print(nombres[n], apellidos[n])
+
+# datos = [4, 6.9, "Patricio", False]
+
+# for d in datos:
+#     print(d)
+
+matrix = [
+    [5,8,3], [79,34,24]
+]
+print(matrix[1][0])
+
