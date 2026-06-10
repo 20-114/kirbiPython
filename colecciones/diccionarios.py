3b1424d3a65ad4fcc4e7821bc83788bc4bbbba29
rafa <rafael.est07@gmail.com>
2026-06-10 17:02:04 -0400

    creando tienda de compras con listas y diccionario 1.1

--- colecciones/diccionarios.py
+++ colecciones/diccionarios.py
@@ -104,86 +104,21 @@ productosDicc = {
     3 : {'nombre' : 'Cebolla', 'precio' : 3000}
 }
 
-# def agregarVegetal():
-#     print("-"*25)
-#     agregarVeg = input("Ingrese un vegetal: ")
-#     agregarPrecio = int(input("Ingrese el precio del nuevo vegetal: "))
-#     llave = list(productosDicc.keys())[-1]   
-#     productosDicc[llave+1] = {'nombre': agregarVeg, 'precio' : agregarPrecio}
-
-# def eliminarVegetal():
-#     print("-"*25)
-#     mostrarVegetales()
-#     eliminar = int(input("Indica que el número de vegetal que deseas eliminar: "))
-#     del productosDicc[eliminar]
-
-# def actualizarVegetales():
-#     print("-"*25)
-#     mostrarVegetales()
-#     update = int(input("Ingresa el número del vegetal que deseas actualizar: "))
-#     upd = int(input("1.-Actualizar nombre \n2.-Actualizar precio \n : "))
-#     match upd:
-#         case 1:
-#             updateVegetal = input("Ingrese el nuevo vegetal: ")
-#             productosDicc[update]['nombre'] = updateVegetal
-#         case 2:
-#             updatePrecioVegetal = int(input("Ingrese el nuevo precio: "))
-#             productosDicc[update]['precio'] = updatePrecioVegetal
-#         case _:
-#             print("Inrgeso Inválido")
-
-# def mostrarVegetales():
-#     print("-"*25)
-#     for num, nombre in productosDicc.items():
-#         print(f"{num}.- {nombre['nombre']} = {nombre['precio']}")
-
-# def vegetalesMenuDicc():
-#     while True:
-#         print("-"*25)
-#         try:
-#             print("1. Agergar vegetales")
-#             print("2. Eliminar vegetales")
-#             print("3. Actualizar vegetales")
-#             print("4. Mostrar vegetales")
-#             print("5.  Salir")
-#             op = int(input("Selecciona un opción: "))
-#             match op:
-#                 case 1:
-#                     agregarVegetal()
-#                 case 2:
-#                     eliminarVegetal()
-#                 case 3:
-#                     actualizarVegetales()
-#                 case 4:
-#                     mostrarVegetales()
-#                 case 5:
-#                     break
-#                 case _:
-#                     print("Opción Inválida")
-#         except:
-#             print("Opción indálida")
-
-# vegetalesMenuDicc()
-
-'''lista con diccionario'''
-
-productosListDicc = [
-    {'nombre' : 'Maracuya', 'precio' : 3000},
-    {'nombre' : 'Pera', 'precio' : 3000},
-    {'nombre' : 'Cebolla', 'precio' : 3000}
-]
-
+print(productosDicc.keys())
+print(productosDicc.values())
+print(productosDicc.items())
 def agregarVegetal():
     print("-"*25)
     agregarVeg = input("Ingrese un vegetal: ")
     agregarPrecio = int(input("Ingrese el precio del nuevo vegetal: "))
-    productosListDicc.append({'nombre': agregarVeg, 'precio' : agregarPrecio})
+    llave = list(productosDicc.keys())[-1]   
+    productosDicc[llave+1] = {'nombre': agregarVeg, 'precio' : agregarPrecio}
 
 def eliminarVegetal():
     print("-"*25)
     mostrarVegetales()
     eliminar = int(input("Indica que el número de vegetal que deseas eliminar: "))
-    productosListDicc.pop(eliminar)
+    del productosDicc[eliminar]
 
 def actualizarVegetales():
     print("-"*25)
@@ -193,19 +128,19 @@ def actualizarVegetales():
     match upd:
         case 1:
             updateVegetal = input("Ingrese el nuevo vegetal: ")
-            productosListDicc[update]['nombre'] = updateVegetal
+            productosDicc[update]['nombre'] = updateVegetal
         case 2:
             updatePrecioVegetal = int(input("Ingrese el nuevo precio: "))
-            productosListDicc[update]['precio'] = updatePrecioVegetal
+            productosDicc[update]['precio'] = updatePrecioVegetal
         case _:
             print("Inrgeso Inválido")
- 
+
 def mostrarVegetales():
     print("-"*25)
-    for nombre in productosListDicc:
-        print(f"{productosListDicc.index(nombre)}.- {nombre['nombre']} = {nombre['precio']}")
+    for num, nombre in productosDicc.items():
+        print(f"{num}.- {nombre['nombre']} = {nombre['precio']}")
 
-def vegetalesMenuList():
+def vegetalesMenuDicc():
     while True:
         print("-"*25)
         try:
@@ -214,7 +149,7 @@ def vegetalesMenuList():
             print("3. Actualizar vegetales")
             print("4. Mostrar vegetales")
             print("5.  Salir")
-            op = int(input("Selecciona un aopción: "))
+            op = int(input("Selecciona un opción: "))
             match op:
                 case 1:
                     agregarVegetal()
@@ -231,6 +166,74 @@ def vegetalesMenuList():
         except:
             print("Opción indálida")
 
-vegetalesMenuList()
+vegetalesMenuDicc()
+
+'''lista con diccionario'''
+
+# productosListDicc = [
+#     {'nombre' : 'Maracuya', 'precio' : 3000},
+#     {'nombre' : 'Pera', 'precio' : 3000},
+#     {'nombre' : 'Cebolla', 'precio' : 3000}
+# ]
+
+# def agregarVegetal():
+#     print("-"*25)
+#     agregarVeg = input("Ingrese un vegetal: ")
+#     agregarPrecio = int(input("Ingrese el precio del nuevo vegetal: "))
+#     productosListDicc.append({'nombre': agregarVeg, 'precio' : agregarPrecio})
+
+# def eliminarVegetal():
+#     print("-"*25)
+#     mostrarVegetales()
+#     eliminar = int(input("Indica que el número de vegetal que deseas eliminar: "))
+#     productosListDicc.pop(eliminar)
+
+# def actualizarVegetales():
+#     print("-"*25)
+#     mostrarVegetales()
+#     update = int(input("Ingresa el número del vegetal que deseas actualizar: "))
+#     upd = int(input("1.-Actualizar nombre \n2.-Actualizar precio \n : "))
+#     match upd:
+#         case 1:
+#             updateVegetal = input("Ingrese el nuevo vegetal: ")
+#             productosListDicc[update]['nombre'] = updateVegetal
+#         case 2:
+#             updatePrecioVegetal = int(input("Ingrese el nuevo precio: "))
+#             productosListDicc[update]['precio'] = updatePrecioVegetal
+#         case _:
+#             print("Inrgeso Inválido")
+ 
+# def mostrarVegetales():
+#     print("-"*25)
+#     for nombre in productosListDicc:
+#         print(f"{productosListDicc.index(nombre)}.- {nombre['nombre']} = {nombre['precio']}")
+
+# def vegetalesMenuList():
+#     while True:
+#         print("-"*25)
+#         try:
+#             print("1. Agergar vegetales")
+#             print("2. Eliminar vegetales")
+#             print("3. Actualizar vegetales")
+#             print("4. Mostrar vegetales")
+#             print("5.  Salir")
+#             op = int(input("Selecciona un aopción: "))
+#             match op:
+#                 case 1:
+#                     agregarVegetal()
+#                 case 2:
+#                     eliminarVegetal()
+#                 case 3:
+#                     actualizarVegetales()
+#                 case 4:
+#                     mostrarVegetales()
+#                 case 5:
+#                     break
+#                 case _:
+#                     print("Opción Inválida")
+#         except:
+#             print("Opción indálida")
+
+# vegetalesMenuList()
 
 
---
+++ colecciones/pokemons.py
@@ -0,0 +1,16 @@
+pokemons = {
+    1:{'nombre': 'Eevee',
+       'nlv' : 14,
+       'hp': 32 ,
+       'atk':
+       {
+           1:{'nombre':'placaje', 'daño': 16-24},
+           2:{'nombre':'placaje', 'daño': 16-24},
+           3:{'nombre':'placaje', 'daño': 16-24},
+           4:{'nombre':'placaje', 'daño': 16-24}
+       },
+       'def': 10 ,
+       'type' : 'normal',
+       'vel' : 12
+                },
+}
